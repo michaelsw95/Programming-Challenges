fc891b0b5a7d4c260167156f3f473f37fc7786d6
Michael Hancock <michael.hancock@protonmail.com>
2019-03-08 23:28:54 +0000

    Add solution for ProjectEuler Problem 2

---
+++ ProjectEuler/Problem-2/program.js
@@ -0,0 +1,16 @@
+const sumOfEvenFibonacciNumbers = (lastFibNumber, currentFibNumber, sum, maxValue) => {
+    const nextFibNumber = lastFibNumber + currentFibNumber;
+
+    if (nextFibNumber > maxValue) {
+        return sum;
+    }
+
+    const newSum = nextFibNumber % 2 === 0 ?
+        nextFibNumber + sum : sum;
+
+    return sumOfEvenFibonacciNumbers(currentFibNumber, nextFibNumber, newSum, maxValue);
+}
+  
+const result = sumOfEvenFibonacciNumbers(1, 1, 0, 4000000);
+
+console.log('Project Euler - Problem 2: ' + result);
