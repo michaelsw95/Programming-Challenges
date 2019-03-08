9f8c4dbbdfe2e7ae1c00c88129778f711bbc1572
Michael Hancock <michael.hancock@protonmail.com>
2019-03-08 22:52:06 +0000

    Add ProjectEuler folder and solution to Problem 1

---
+++ ProjectEuler/Problem-1/program.js
@@ -0,0 +1,14 @@
+const sumOfAllMultiples = (multiple, upperLimit, currentSum = 0, currentIteration = 0) => {
+    if (upperLimit === currentIteration) {
+        return currentSum;
+    }
+
+    const newSum = currentIteration % multiple === 0 ?
+        currentSum + currentIteration : currentSum;
+
+    return sumOfAllMultiples(multiple, upperLimit, newSum, currentIteration + 1);
+}
+
+const result = sumOfAllMultiples(3, 1000) + sumOfAllMultiples(5, 1000);
+
+console.log('Project Euler - Problem 1: ' + result);
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Advent of Code 2018 - https://adventofcode.com/2018
+Project Euler - https://projecteuler.net/
