3be09a0074c66bc6234b3b871e8ae215622d4167
Michael Hancock <michael.hancock@protonmail.com>
2019-12-01 22:00:35 +0000

    Add solution for AdventOfCode2019 Day 1

---
+++ AdventOfCode2019/Day-01-Part-01/Program.fs
@@ -0,0 +1,35 @@
+﻿open System
+open System.IO
+
+let getInput (filePath: string) =
+    File.ReadAllLines(filePath) |> Seq.cast<string>
+
+let calculateFuelForModule moduleMass =
+    let moduleMassAsFloat = float moduleMass
+    let mass = moduleMassAsFloat / float 3
+    let roundedMass = int (Math.Floor mass)
+    roundedMass - 2
+
+let rec calculateFuelForAllModules (moduleMasses: seq<string>) currentTotal =
+    if Seq.isEmpty(moduleMasses) then
+        currentTotal
+    else
+        let current = moduleMasses |> Seq.head |> int
+        let currentMass = calculateFuelForModule current
+
+        calculateFuelForAllModules
+            (Seq.skip 1 moduleMasses)
+            (currentMass + currentTotal)
+
+[<EntryPoint>]
+let main argv =
+    if argv.Length <> 1 then
+        raise (ArgumentException "Input path was not provider")
+
+    let inputPath = argv |> Seq.head
+    let input = getInput inputPath
+    let result = calculateFuelForAllModules input 0
+
+    Console.WriteLine ("Day 1 - Part 1: " + string result)
+
+    0
