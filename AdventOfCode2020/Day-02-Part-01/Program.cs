30c9ee55052a3f8ca7ce1551d2a70c5c7841dc35
Michael Hancock <michael.hancock@protonmail.com>
2020-12-28 20:58:04 +0000

    Add solution for AdventOfCode2020 Day 2

---
+++ AdventOfCode2020/Day-02-Part-01/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+var validPasswords = GetValidPasswords("./input.txt");
+
+Console.WriteLine($"Day 2 - Part 1: {validPasswords.Length}");
+
+Password[] GetValidPasswords(string filePath)
+{
+    Password ParsePassword(string password)
+    {
+        var halfs = password.Split(": ");
+
+        var character = halfs[0].Last();
+        var requiredNumberSubString = halfs[0]
+            .Substring(0, halfs[0].Length - 2)
+            .Split("-");
+
+        var lowerBound = int.Parse(requiredNumberSubString[0]);
+        var upperBound = int.Parse(requiredNumberSubString[1]);
+
+        return new Password(character, halfs[1], lowerBound, upperBound);
+    }
+
+    bool IsValidPassword(Password password)
+    {
+        var countOfRequiredLetter = password.PasswordText
+            .Where(o => o == password.LetterRequired)
+            .Count();
+
+        return password.RequiredCount.lower <= countOfRequiredLetter
+            && password.RequiredCount.upper >= countOfRequiredLetter;
+    }
+
+    return File.ReadAllLines(filePath)
+        .Select(ParsePassword)
+        .Where(IsValidPassword)
+        .ToArray();
+}
+    
+record Password
+{
+    public Password(char letterRequired, string password, int lowerCount, int upperCount)
+    {
+        LetterRequired = letterRequired;
+        RequiredCount = (lowerCount, upperCount);
+        PasswordText = password;
+    }
+
+    public char LetterRequired { get; init; }
+    public (int lower, int upper) RequiredCount { get; init; }
+    public string PasswordText { get; init; }
+}
---
+++ AdventOfCode2020/Day-02-Part-02/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+var validPasswords = GetValidPasswords("./input.txt");
+
+Console.WriteLine($"Day 2 - Part 2: {validPasswords.Length}");
+
+Password[] GetValidPasswords(string filePath)
+{
+    Password ParsePassword(string password)
+    {
+        var halfs = password.Split(": ");
+
+        var character = halfs[0].Last();
+        var requiredIndexesSubString = halfs[0]
+            .Substring(0, halfs[0].Length - 2)
+            .Split("-");
+
+        var lowerBound = int.Parse(requiredIndexesSubString[0]);
+        var upperBound = int.Parse(requiredIndexesSubString[1]);
+
+        return new Password(character, halfs[1], lowerBound - 1, upperBound - 1);
+    }
+
+    bool IsValidPassword(Password password)
+    {
+        var countOfRequiredLetter = password.PasswordText
+            .Where(o => o == password.LetterRequired)
+            .Count();
+
+        return
+            (password.PasswordText[password.RequiredIndexes.lower] == password.LetterRequired &&
+            password.PasswordText[password.RequiredIndexes.upper] != password.LetterRequired) || 
+            (password.PasswordText[password.RequiredIndexes.upper] == password.LetterRequired &&
+            password.PasswordText[password.RequiredIndexes.lower] != password.LetterRequired);
+    }
+
+    return File.ReadAllLines(filePath)
+        .Select(ParsePassword)
+        .Where(IsValidPassword)
+        .ToArray();
+}
+    
+record Password
+{
+    public Password(char letterRequired, string password, int lower, int upper)
+    {
+        LetterRequired = letterRequired;
+        RequiredIndexes = (lower, upper);
+        PasswordText = password;
+    }
+
+    public char LetterRequired { get; init; }
+    public (int lower, int upper) RequiredIndexes { get; init; }
+    public string PasswordText { get; init; }
+}
