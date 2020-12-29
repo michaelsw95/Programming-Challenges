b445544ccd42c4f4e319fdc04ddb5bad38aa4f0e
Michael Hancock <michael.hancock@protonmail.com>
2020-12-29 00:13:07 +0000

    Add solution for AdventOfCode2020 Day 3

---
+++ AdventOfCode2020/Day-03-Part-01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+var map = File.ReadAllLines("input.txt");
+
+const char tree = '#';
+var treeCount = 0;
+(int x, int y) step = (1, 3);
+(int x, int y) currentPosition = (0, 0);
+
+while (currentPosition.x < map.Length)
+{
+    if (map[currentPosition.x][currentPosition.y % map[0].Length] == tree)
+    {
+        treeCount += 1;
+    }
+
+    currentPosition.x += step.x;
+    currentPosition.y += step.y;
+}
+
+Console.WriteLine($"Day 3 - Part 1: {treeCount}");
---
+++ AdventOfCode2020/Day-03-Part-02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+var map = File.ReadAllLines("input.txt");
+
+long treeCount = GetCountOfStepEncounters((1, 1), map, '#');
+treeCount *= GetCountOfStepEncounters((1, 3), map, '#');
+treeCount *= GetCountOfStepEncounters((1, 5), map, '#');
+treeCount *= GetCountOfStepEncounters((1, 7), map, '#');
+treeCount *= GetCountOfStepEncounters((2, 1), map, '#');
+
+Console.WriteLine($"Day 3 - Part 2: {treeCount}");
+
+int GetCountOfStepEncounters((int x, int y) step, string[] map, char target)
+{
+    (int x, int y) currentPosition = (0, 0);
+    var targetCount = 0;
+
+    while (currentPosition.x < map.Length)
+    {
+        if (map[currentPosition.x][currentPosition.y % map[0].Length] == target)
+        {
+            targetCount += 1;
+        }
+
+        currentPosition.x += step.x;
+        currentPosition.y += step.y;
+    }
+
+    return targetCount;
+}
