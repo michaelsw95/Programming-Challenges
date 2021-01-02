fe9976555b76e6c812b82dc01f06e6565b0557a7
Michael Hancock <michael.hancock@protonmail.com>
2021-01-02 21:32:27 +0000

    Add solution for AdventOfCode2020 Day 6

---
+++ AdventOfCode2020/Day-06-Part-01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+var sumOfAnswers = File
+    .ReadAllText("input.txt")
+    .Split(new string[] { Environment.NewLine + Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries)
+    .Select(CountDistinctAnswers)
+    .Sum();
+
+Console.WriteLine($"Day 6 - Part 1: {sumOfAnswers}");
+
+int CountDistinctAnswers(string answer) => answer.ToCharArray()
+    .Where(answer => !Char.IsWhiteSpace(answer))
+    .GroupBy(answer => answer)
+    .Count();
---
+++ AdventOfCode2020/Day-06-Part-02/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+var sumOfAnswers = File
+    .ReadAllText("input.txt")
+    .Split(new string[] { Environment.NewLine + Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries)
+    .Select(CountSharedAnswers)
+    .Sum();
+
+Console.WriteLine($"Day 6 - Part 2: {sumOfAnswers}");
+
+int CountSharedAnswers(string groupAnswers)
+{
+    var answersPerPerson = groupAnswers.Split(Environment.NewLine);
+
+    var answersInContention = answersPerPerson
+        .First()
+        .ToCharArray()
+        .ToHashSet();
+
+    foreach (var answer in answersPerPerson.First())
+    {
+        for (var i = 1; i < answersPerPerson.Length; i++)
+        {
+            if (!answersPerPerson[i].Contains(answer))
+            {
+                answersInContention.Remove(answer);
+                break;
+            }
+        }
+    }
+
+    return answersInContention.Count;
+}
