ff6b3c1da832e4ca02611f086d239fb95192494a
Michael Hancock <michael.hancock@protonmail.com>
2021-01-02 20:52:59 +0000

    Add solution for AdventOfCode2020 Day 5

---
+++ AdventOfCode2020/Day-05-Part-01/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+var highestBoaringPassId = File.ReadAllLines("input.txt")
+    .Select(pass => GetPositionId(new Position
+    {
+        Row = EvaluateBinarySpacePartition(pass.Substring(0, 7), 'B', 'F', 128),
+        Seat = EvaluateBinarySpacePartition(pass.Substring(6), 'R', 'L', 8)
+    }))
+    .Max();
+
+Console.WriteLine($"Day 5 - Part 1: {highestBoaringPassId}");
+
+int EvaluateBinarySpacePartition(string positionString, char upperId, char lowerId, int partitionUpperBound)
+{
+    (int lowBound, int upperBound) rangeInConsideration = (0, partitionUpperBound - 1);
+    foreach (var direction in positionString)
+    {
+        var midPoint = (float)(rangeInConsideration.lowBound + rangeInConsideration.upperBound) / 2;
+
+        if (direction == lowerId)
+        {
+            rangeInConsideration.upperBound = (int)Math.Floor(midPoint);
+        }
+        else if (direction == upperId)
+        {
+            rangeInConsideration.lowBound = (int)Math.Ceiling(midPoint);
+        }
+    }
+
+    return rangeInConsideration.lowBound;
+}
+
+int GetPositionId(Position position) => (position.Row * 8) + position.Seat;
+
+record Position
+{
+    public int Row { get; init; }
+    public int Seat { get; init; }
+}
---
+++ AdventOfCode2020/Day-05-Part-02/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+const int numberOfRows = 128;
+const int numberOfColumns = 8;
+
+var boardingPassDefinitions = File.ReadAllLines("input.txt");
+var boardingPasses = new HashSet<(int row, int column)>(boardingPassDefinitions.Length);
+var allPassIds = new HashSet<int>(boardingPassDefinitions.Length);
+
+foreach (var pass in boardingPassDefinitions)
+{
+    var row = EvaluateBinarySpacePartition(pass.Substring(0, numberOfColumns - 1), 'B', 'F', numberOfRows);
+    var column = EvaluateBinarySpacePartition(pass.Substring(numberOfColumns - 2), 'R', 'L', numberOfColumns);
+    var id = GetPositionId(row, column);
+
+    boardingPasses.Add((row, column));
+    allPassIds.Add(id);
+}
+
+var emptySeatId = GetEmptyPostionId(boardingPasses, allPassIds);
+
+Console.WriteLine($"Day 5 - Part 2: {emptySeatId}");
+
+int EvaluateBinarySpacePartition(string positionString, char upperId, char lowerId, int partitionUpperBound)
+{
+    (int lowBound, int upperBound) rangeInConsideration = (0, partitionUpperBound - 1);
+
+    foreach (var direction in positionString)
+    {
+        var midPoint = (float)(rangeInConsideration.lowBound + rangeInConsideration.upperBound) / 2;
+
+        if (direction == lowerId)
+        {
+            rangeInConsideration.upperBound = (int)Math.Floor(midPoint);
+        }
+        else if (direction == upperId)
+        {
+            rangeInConsideration.lowBound = (int)Math.Ceiling(midPoint);
+        }
+    }
+
+    return rangeInConsideration.lowBound;
+}
+
+int? GetEmptyPostionId(HashSet<(int row, int column)> boardingPasses, HashSet<int> allPassIds)
+{
+    for (var row = 0; row < numberOfRows; row++) 
+    {
+        for (var column = 0 ; column < numberOfColumns; column++)
+        {
+            if (!boardingPasses.Contains((row, column)))
+            {
+                var candidateId = GetPositionId(row, column);
+
+                if (allPassIds.Contains(candidateId + 1) && allPassIds.Contains(candidateId - 1))
+                {
+                    return candidateId;
+                }
+            }
+        }
+    }
+
+    return null;
+}
+
+int GetPositionId(int row, int column) => (row * 8) + column;
