bb6c001f5a0c4d61b0ad0e1dfc5e9d514c85f5ff
Michael Hancock <michael.hancock@protonmail.com>
2021-11-30 23:44:55 +0000

    Add solution for ProjectEuler Problem 12

---
+++ ProjectEuler/Problem-12/Program.cs
@@ -0,0 +1,38 @@
+﻿const int targetCountOfFactors = 501;
+
+var triangleNumberPosition = 0;
+var triangleNumber = 0;
+var countOfFactors = 0;
+
+do {
+    triangleNumberPosition++;
+
+    triangleNumber += triangleNumberPosition;
+
+    countOfFactors = GetCountOfFactors(triangleNumber);
+} while (countOfFactors <= targetCountOfFactors);
+
+Console.WriteLine($"Project Euler - Problem 12: {triangleNumber}");
+
+int GetCountOfFactors(int numberToCountFactors)
+{
+    var countOfFactors = 0;
+    var sqrtOfTarget = Math.Ceiling(Math.Sqrt(numberToCountFactors));
+    
+    if (NumberIsExactSquare(sqrtOfTarget, numberToCountFactors))
+    {
+        countOfFactors++;
+    }
+
+    for (var i = Convert.ToInt32(sqrtOfTarget); i > 0; i--)
+    {
+        if (numberToCountFactors % i == 0)
+        {
+            countOfFactors += 2;
+        }
+    }
+
+    return countOfFactors;
+
+    bool NumberIsExactSquare(double sqrtOfTarget, int target) => sqrtOfTarget * sqrtOfTarget == target;
+}
