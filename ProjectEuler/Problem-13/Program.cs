2576c733ac068af75c83bac58e12de5961a61abc
Michael Hancock <michael.hancock@protonmail.com>
2021-12-01 19:17:30 +0000

    Add solution for ProjectEuler Problem 13

---
+++ ProjectEuler/Problem-13/Program.cs
@@ -0,0 +1,7 @@
+﻿var truncatedSum = File.ReadAllLines("./input.txt")
+    .Select(line => long.Parse(line.Substring(0, 11)))
+    .Sum()
+    .ToString()
+    .Substring(0, 10);
+
+Console.WriteLine($"Project Euler - Problem 12: {truncatedSum}");
