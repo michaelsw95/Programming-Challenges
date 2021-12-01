d022a08f4bf54d3fdfec30f3305ea63cd4b5640b
Michael Hancock <michael.hancock@protonmail.com>
2021-12-01 13:29:10 +0000

    Add solution for AdventOfCode2021 Day 1 Part 1

---
+++ AdventOfCode2021/Day-01-Part-01/Program.cs
@@ -0,0 +1,8 @@
+﻿var sonarSweaps = File.ReadAllLines("./input.txt")
+    .Select(line => int.Parse(line))
+    .ToArray();
+
+var countOfIncreasingDepths = Enumerable.Range(1, sonarSweaps.Length - 1)
+    .Count(i => sonarSweaps[i] > sonarSweaps[i - 1]);
+
+Console.WriteLine($"Day 1 - Part 1: {countOfIncreasingDepths}");
