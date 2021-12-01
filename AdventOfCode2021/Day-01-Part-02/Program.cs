3ae10cc322b3a8527cd9c26cf2a202162eef1124
Michael Hancock <michael.hancock@protonmail.com>
2021-12-01 13:29:40 +0000

    Add solution for AdventOfCode2021 Day 1 Part 2

---
+++ AdventOfCode2021/Day-01-Part-02/Program.cs
@@ -0,0 +1,18 @@
+﻿var sonarSweaps = File.ReadAllLines("./input.txt")
+    .Select(line => int.Parse(line))
+    .ToArray();
+
+var countOfIncreasingGroupDepths = 0;
+
+for (var i = 0; i < sonarSweaps.Length - 3; i++)
+{
+    var currentGroupSum = sonarSweaps[i] + sonarSweaps[i + 1] + sonarSweaps[i + 2];
+    var nextGroupSum = sonarSweaps[i + 1] + sonarSweaps[i + 2] + sonarSweaps[i + 3];
+
+    if (currentGroupSum < nextGroupSum)
+    {
+        countOfIncreasingGroupDepths++;
+    }
+}
+
+Console.WriteLine($"Day 1 - Part 2: {countOfIncreasingGroupDepths}");
