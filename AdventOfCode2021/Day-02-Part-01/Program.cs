dcb27c02c995cdfec762338ab30469c094e7ff71
Michael Hancock <michael.hancock@protonmail.com>
2021-12-02 13:18:26 +0000

    Add solution for AdventOfCode2021 Day 2 Part 1

---
+++ AdventOfCode2021/Day-02-Part-01/Program.cs
@@ -0,0 +1,9 @@
+﻿var instructions = File.ReadAllLines("./input.txt")
+    .Select(line => line.Split(" "))
+    .GroupBy(lineParts => lineParts.First(), lineParts => int.Parse(lineParts.Last()))
+    .ToDictionary(group => group.Key, group => group.Sum());
+
+var depth = instructions["down"] - instructions["up"];
+var forward = instructions["forward"];
+
+Console.WriteLine($"Day 2 - Part 1: {depth * forward}");
