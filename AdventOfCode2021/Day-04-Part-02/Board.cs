2e90b68f2dd144c4d597d9ef294b4570ab7fd4b0
Michael Hancock <michael.hancock@protonmail.com>
2021-12-04 22:20:57 +0000

    Add solution for AdventOfCode2021 Day 4 Part 2

---
+++ AdventOfCode2021/Day-04-Part-02/Board.cs
@@ -0,0 +1,78 @@
+public class Board
+{
+    public static int boardSize = 5;
+    private readonly List<(int guess, bool found)[]> _board;
+    private int _lastSeenGuess;
+    public bool HasWon { get; set; }
+
+    public Board()
+    {
+        _board = new List<(int, bool)[]>(5);
+    }
+
+    public void AddRow(IEnumerable<int> row)
+    {
+        _board.Add(row.Select(guess => (guess, false)).ToArray());
+    }
+
+    public void CheckNumber(int guess)
+    {
+        _lastSeenGuess = guess;
+
+        for (int i = 0; i < _board.Count; i++)
+        {
+            for (int j = 0; j < _board[i].Length; j++)
+            {
+                if (_board[i][j].guess == guess) 
+                {
+                    _board[i][j].found = true;
+                }
+            }
+        }
+    }
+
+    public bool IsWinner()
+    {
+        for (var i = 0; i < boardSize; i++)
+        {
+            var rowWins = _board[i].All(boardItem => boardItem.found);
+
+            if (rowWins)
+            {
+                return true;
+            }
+        }
+
+        for (var i = 0; i < boardSize; i++)
+        {
+            var column = new (int, bool found)[5] {
+                _board[0][i], _board[1][i], _board[2][i], _board[3][i], _board[4][i]
+            };
+
+            if (column.All(boardItem => boardItem.found))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public int GetScore()
+    {
+        var unmarkedNumbers = new List<int>();
+
+        foreach (var row in _board)
+        {
+            foreach (var item in row)
+            {
+                if (!item.found)
+                {
+                    unmarkedNumbers.Add(item.guess);
+                }
+            }
+        }
+
+        return unmarkedNumbers.Sum() * _lastSeenGuess;
+    }
+}
---
+++ AdventOfCode2021/Day-04-Part-02/Program.cs
@@ -0,0 +1,50 @@
+﻿var gameInput = File.ReadAllLines("./input.txt");
+
+var drawnNumbers = gameInput.First().Split(",").Select(draw => int.Parse(draw)).ToArray();
+
+var boards = new List<Board>();
+
+for (var i = 2; i < gameInput.Length; i += (Board.boardSize + 1))
+{
+    var board = new Board();
+
+    for (var j = 0; j < Board.boardSize; j++)
+    {
+        var row = gameInput[i + j].Split(" ")
+            .Where(draw => !string.IsNullOrWhiteSpace(draw))
+            .Select(draw => int.Parse(draw));
+
+        board.AddRow(row);
+    }
+
+    boards.Add(board);
+}
+
+var winnerScore = 0;
+var numberOfWinners = 0;
+foreach (var number in drawnNumbers)
+{
+    foreach (var board in boards)
+    {
+        if (board.HasWon)
+        {
+            continue;
+        }
+
+        board.CheckNumber(number);
+
+        if (board.IsWinner())
+        {
+            board.HasWon = true;
+            numberOfWinners++;
+
+            if (numberOfWinners == boards.Count)
+            {
+                winnerScore = board.GetScore();
+                break;
+            }
+        }
+    }
+}
+
+Console.WriteLine($"Day 4 - Part 2: {winnerScore}");
