1d1548f5f8ff590b60d21e5cd8549f2d84f60170
Michael Hancock <michael.hancock@protonmail.com>
2021-12-06 19:50:43 +0000

    Add solution for AdventOfCode2021 Day 6 Part 2

---
+++ AdventOfCode2021/Day-06-Part-02/Program.cs
@@ -0,0 +1,34 @@
+﻿const int newFishTimer = 8;
+const int targetGeneration = 256;
+
+var allFish = File.ReadAllText("input.txt")
+    .Split(",")
+    .Select(fishTimer => int.Parse(fishTimer))
+    .ToList();
+
+var fishBuckets = Enumerable.Range(0, newFishTimer + 1)
+    .ToDictionary(bucket => bucket, _ => 0L);
+
+foreach (var fish in allFish)
+{
+    fishBuckets[fish]++;
+}
+
+for (var generation = 0; generation < targetGeneration; generation++)
+{
+    var updatedBuckets = new Dictionary<int, long>();
+
+    for (var i = 0; i < newFishTimer; i++)
+    {
+        updatedBuckets[i] = fishBuckets[i + 1];
+    }
+
+    updatedBuckets[newFishTimer] = fishBuckets[0];
+    updatedBuckets[newFishTimer - 2] += fishBuckets[0];
+
+    fishBuckets = new Dictionary<int, long>(updatedBuckets);
+}
+
+var total = fishBuckets.Select(bucket => bucket.Value).Sum();
+
+Console.WriteLine($"Day 6 - Part 2: {total}");
