57d1fa75f9846d5b06d8674e2c982de3ea9d046b
Michael Hancock <michael.hancock@protonmail.com>
2021-12-06 19:45:17 +0000

    Add solution for AdventOfCode2021 Day 6 Part 1

---
+++ AdventOfCode2021/Day-06-Part-01/Program.cs
@@ -0,0 +1,27 @@
+﻿var fish = File.ReadAllText("input.txt")
+    .Split(",")
+    .Select(fishTimer => int.Parse(fishTimer))
+    .ToList();
+
+for (var generation = 0; generation < 80; generation++)
+{
+    var newFishToAddAfterGeneration = 0;
+    for (var i = 0; i < fish.Count; i++)
+    {
+        if (fish[i] == 0)
+        {
+            fish[i] = 7;
+
+            newFishToAddAfterGeneration++;
+        }
+
+        fish[i] = fish[i] - 1;
+    }
+
+    for (var i = 0; i < newFishToAddAfterGeneration; i++)
+    {
+        fish.Add(8);
+    }
+}
+
+Console.WriteLine($"Day 6 - Part 1: {fish.Count}");
