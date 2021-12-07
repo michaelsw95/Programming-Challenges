10ec399cb9b0cd7e7fc0bb79212d14fcb65ed7e6
Michael Hancock <michael.hancock@protonmail.com>
2021-12-07 08:57:50 +0000

    Add solution for AdventOfCode2021 Day 7 Part 2

---
+++ AdventOfCode2021/Day-07-Part-02/Program.cs
@@ -0,0 +1,27 @@
+﻿var crabPositions = File.ReadAllText("./input.txt")
+    .Split(",")
+    .Select(position => int.Parse(position))
+    .ToArray();
+
+(int position, int fuelCost) bestFoundPosition = (0, 0);
+for (var candidatePosition = 0; candidatePosition < crabPositions.Max(); candidatePosition++)
+{
+    var fuelCost = 0;
+    foreach (var crab in crabPositions)
+    {
+        var lower = Math.Min(crab, candidatePosition);
+        var upper = Math.Max(crab, candidatePosition);
+
+        var steps = Enumerable.Range(0, (upper - lower) + 1);
+
+        fuelCost += steps.Sum();
+    }
+
+    if (bestFoundPosition.fuelCost <= 0 || bestFoundPosition.fuelCost > fuelCost)
+    {
+        bestFoundPosition.position = candidatePosition;
+        bestFoundPosition.fuelCost = fuelCost;
+    }
+}
+
+Console.WriteLine($"Day 7 - Part 2: {bestFoundPosition.fuelCost}");
