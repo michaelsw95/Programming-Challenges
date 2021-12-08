f42f2dd4b9f7ef50274be63a370a8e4335d73912
Michael Hancock <michael.hancock@protonmail.com>
2021-12-08 22:20:56 +0000

    Add solution for AdventOfCode2021 Day 8 Part 1

---
+++ AdventOfCode2021/Day-08-Part-01/Program.cs
@@ -0,0 +1,17 @@
+﻿var rawSignalEntries = File.ReadAllLines("./input.txt");
+
+var uniqueSegmentCounts = new HashSet<int>() { 2, 4, 3, 7 };
+
+var countOfIndentifiedNumbers = 0;
+foreach (var line in rawSignalEntries)
+{
+    countOfIndentifiedNumbers += line
+        .Split('|')
+        .Last()
+        .Split(" ")
+        .Where(linePart => !string.IsNullOrWhiteSpace(linePart))
+        .Where(linePart => uniqueSegmentCounts.Contains(linePart.Length))
+        .Count();
+}
+
+Console.WriteLine($"Day 8 - Part 1: {countOfIndentifiedNumbers}");
