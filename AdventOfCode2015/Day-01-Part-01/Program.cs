41d86d25ff2990ded317ef90fc540b4b7c718d71
Michael Hancock <michael.hancock@protonmail.com>
2022-12-04 14:07:33 +0000

    Add solution for AdventOfCode2015 Day 1

---
+++ AdventOfCode2015/Day-01-Part-01/Program.cs
@@ -0,0 +1,6 @@
+﻿var floorInstructions = File.ReadAllText("./input.txt");
+
+var moveUp = floorInstructions.Count(instruction => instruction == '(');
+var moveDown = floorInstructions.Count(instruction => instruction == ')');
+
+Console.WriteLine($"Day 1 - Part 1: {moveUp - moveDown}");
---
+++ AdventOfCode2015/Day-01-Part-02/Program.cs
@@ -0,0 +1,19 @@
+﻿var floorInstructions = File.ReadAllText("./input.txt");
+
+var floorPosition = 0;
+var firstBasementPosition = 0;
+
+const int basementFloor = -1;
+
+for (int i = 0; i < floorInstructions.Length; i++)
+{
+    floorPosition += floorInstructions[i] == '(' ? 1 : -1;
+    
+    if (floorPosition == basementFloor)
+    {
+        firstBasementPosition = i + 1;
+        break;
+    }
+}
+
+Console.WriteLine($"Day 1 - Part 2: {firstBasementPosition}");
