f0b2aedc7341a5d0fac1d0e000cbeb8fb1a2ee59
Michael Hancock <michael.hancock@protonmail.com>
2022-12-04 14:08:07 +0000

    Add solution for AdventOfCode2015 Day 2

---
+++ AdventOfCode2015/Day-02-Part-01/Program.cs
@@ -0,0 +1,28 @@
+﻿var squareFeetOfWrappingPaper = File.ReadAllLines("./input.txt")
+    .Select(ParsePresentDimensions)
+    .Select(GetSurfaceAreaOfPresent)
+    .Sum();
+
+Console.WriteLine($"Day 2 - Part 1: {squareFeetOfWrappingPaper}");
+
+PresentDimensions ParsePresentDimensions(string line)
+{
+    var numbers = line.Split("x").Select(rawNumber => int.Parse(rawNumber)).ToArray();
+
+    return new PresentDimensions(numbers[0], numbers[1], numbers[2]);
+}
+
+int GetSurfaceAreaOfPresent(PresentDimensions present)
+{
+    var sideOne = present.length * present.width;
+    var sideTwo = present.width * present.height;
+    var sideThree = present.height * present.length;
+
+    var smallestSide = GetSmallestNumber(sideOne, sideTwo, sideThree);
+
+    return (2 * sideOne) + (2 * sideTwo) + (2 * sideThree) + smallestSide;
+}
+
+int GetSmallestNumber(params int[] numbers) => numbers.Min();
+
+record PresentDimensions(int length, int width, int height);
---
+++ AdventOfCode2015/Day-02-Part-02/Program.cs
@@ -0,0 +1,31 @@
+﻿var feetOfRibbon = File.ReadAllLines("./input.txt")
+    .Select(ParsePresentDimensions)
+    .Select(GetRequiredLengthOfRibbon)
+    .Sum();
+
+Console.WriteLine($"Day 2 - Part 2: {feetOfRibbon}");
+
+PresentDimensions ParsePresentDimensions(string line)
+{
+    var numbers = line.Split("x").Select(rawNumber => int.Parse(rawNumber)).ToArray();
+
+    return new PresentDimensions(numbers[0], numbers[1], numbers[2]);
+}
+
+int GetRequiredLengthOfRibbon(PresentDimensions present)
+{
+    var presentDimensionsOrdered = GetPresentDimensionsOrdered(present);
+
+    var presentRibbonLength = (2 * presentDimensionsOrdered[0]) + (2 * presentDimensionsOrdered[1]);
+
+    var bowRibbonLength = present.length * present.height * present.width;
+
+    return presentRibbonLength + bowRibbonLength;
+}
+
+int[] GetPresentDimensionsOrdered(PresentDimensions present) => 
+    new int[3] { present.length, present.height, present.width }
+    .OrderBy(dimension => dimension)
+    .ToArray();
+
+record PresentDimensions(int length, int width, int height);
