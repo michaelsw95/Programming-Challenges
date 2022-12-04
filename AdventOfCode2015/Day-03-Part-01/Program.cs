d5eefcc670ea5b72a90d871699e8101c181b25fa
Michael Hancock <michael.hancock@protonmail.com>
2022-12-04 14:09:40 +0000

    Add solution for AdventOfCode2015 Day 3

---
+++ AdventOfCode2015/Day-03-Part-01/Program.cs
@@ -0,0 +1,19 @@
+﻿var instructions = File.ReadAllText("./input.txt");
+
+var position = (x: 0, y: 0);
+var visted = new HashSet<(int x, int y)>(instructions.Length + 1) { position };
+
+foreach (var instruction in instructions)
+{
+    position = instruction switch
+    {
+        '^' => (position.x, position.y + 1),
+        'v' => (position.x, position.y - 1),
+        '<' => (position.x - 1, position.y),
+        '>' => (position.x + 1, position.y)
+    };
+
+    visted.Add(position);
+}
+
+Console.WriteLine($"Day 3 - Part 1: {visted.Count}");
---
+++ AdventOfCode2015/Day-03-Part-02/Program.cs
@@ -0,0 +1,40 @@
+﻿var instructions = File.ReadAllText("./input.txt");
+
+var santaPosition = (x: 0, y: 0);
+var roboSantaPosition = (x: 0, y: 0);
+
+var visted = new HashSet<(int x, int y)>(instructions.Length + 1) { santaPosition };
+var moveNext = MoveNext.Santa;
+
+foreach (var instruction in instructions)
+{
+    if (moveNext == MoveNext.Santa)
+    {
+        santaPosition = GetNewPosition(instruction, santaPosition);
+        visted.Add(santaPosition);
+        moveNext = MoveNext.RoboSanta;
+    }
+    else
+    {
+        roboSantaPosition = GetNewPosition(instruction, roboSantaPosition);
+        visted.Add(roboSantaPosition);
+        moveNext = MoveNext.Santa;
+    }
+}
+
+Console.WriteLine($"Day 3 - Part 2: {visted.Count}");
+
+(int x, int y) GetNewPosition(char instruction, (int x, int y) currentPosition) =>
+    instruction switch
+    {
+        '^' => (currentPosition.x, currentPosition.y + 1),
+        'v' => (currentPosition.x, currentPosition.y - 1),
+        '<' => (currentPosition.x - 1, currentPosition.y),
+        '>' => (currentPosition.x + 1, currentPosition.y)
+    };
+
+enum MoveNext
+{
+    Santa,
+    RoboSanta
+}
