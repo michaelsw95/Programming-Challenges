6949c49c5b3808033521764dcd196310a4bfa72e
Michael Hancock <michael.hancock@protonmail.com>
2022-12-04 15:21:30 +0000

    Add solution for AdventOfCode2022 Day 3 Part 1

---
+++ AdventOfCode2022/Day-03-Part-01/Program.cs
@@ -0,0 +1,22 @@
+﻿var priorityRange = Enumerable
+    .Range('a', 26)
+    .Select(letter => (char)letter)
+    .ToArray();
+
+var prioritySum = File.ReadAllLines("./input.txt")
+    .Select((string line) =>
+    {
+        var compartmentSize = line.Length / 2;
+
+        var compartmentOne = line.Substring(0, compartmentSize).ToHashSet();
+        var compartmentTwo = line.Substring(compartmentSize).ToHashSet();
+
+        var commonItem = compartmentOne.Single(letter => compartmentTwo.Contains(letter));
+
+        return char.IsUpper(commonItem) ? 
+            (Array.IndexOf(priorityRange, char.ToLower(commonItem)) + 1) + priorityRange.Length :
+            Array.IndexOf(priorityRange, commonItem) + 1;
+    })
+    .Sum();
+    
+Console.WriteLine($"Day 3 - Part 1: {prioritySum}");
