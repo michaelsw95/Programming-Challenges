708c3bd65f358ba08923fafdc69e1df384b620fe
Michael Hancock <michael.hancock@protonmail.com>
2022-12-04 14:14:20 +0000

    Add solution for AdventOfCode2015 Day 5

---
+++ AdventOfCode2015/Day-05-Part-01/Program.cs
@@ -0,0 +1,52 @@
+﻿var countOfNiceWords = File.ReadAllLines("./input.txt")
+    .Count(IsNiceWord);
+
+Console.WriteLine($"Day 5 - Part 1: {countOfNiceWords}");
+
+bool IsNiceWord(string word) =>
+    ContainsThreeVowels(word) &&
+    ContainsConsecutiveLetters(word) &&
+    !ContainsBannedStrings(word);
+
+bool ContainsThreeVowels(string word)
+{
+    var vowels = new HashSet<char> { 'a', 'e', 'i', 'o', 'u' };
+
+    var countOfVowels = 0;
+    foreach (var letter in word)
+    {
+        if (vowels.Contains(letter))
+        {
+            countOfVowels++;
+
+            if (countOfVowels >= 3)
+            {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+bool ContainsConsecutiveLetters(string word)
+{
+    var lastLetter = default(char);
+
+    foreach (var letter in word)
+    {
+        if (lastLetter == letter)
+        {
+            return true;
+        }
+
+        lastLetter = letter;
+    }
+
+    return false;
+}
+
+bool ContainsBannedStrings(string word)
+{
+    return new string[] { "ab", "cd", "pq", "xy" }.Any(bannedWord => word.Contains(bannedWord));
+}
---
+++ AdventOfCode2015/Day-05-Part-02/Program.cs
@@ -0,0 +1,49 @@
+﻿var countOfNiceWords = File.ReadAllLines("./input.txt")
+    .Count(IsNiceWord);
+
+Console.WriteLine($"Day 5 - Part 2: {countOfNiceWords}");
+
+bool IsNiceWord(string word) =>
+    ContainsTwoLettersThatAppearTwice(word) &&
+    ContainsPairWithExactlyOneLetterBetween(word);
+
+bool ContainsTwoLettersThatAppearTwice(string word)
+{
+    var pairs = new List<(char, char)>();
+
+    for (var i = 0; i < word.Length - 1; i++)
+    {
+        pairs.Add((word[i], word[i + 1]));
+    }
+
+    for (var i = 0; i < pairs.Count; i++)
+    {
+        var candidatePair = pairs[i];
+
+        for (var j = 0; j < pairs.Count; j++)
+        {
+            if (candidatePair == pairs[j] && PairIsNotOverlapping(i, j))
+            {
+                return true;
+            }
+        }
+    }
+
+    return false;
+
+    bool PairIsNotOverlapping(int indexOne, int indexTwo) =>
+        indexOne > indexTwo + 1 || indexOne < indexTwo - 1;
+}
+
+bool ContainsPairWithExactlyOneLetterBetween(string word)
+{
+    for (var i = 0; i < word.Length - 2; i++)
+    {
+        if (word[i] == word[i + 2])
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
