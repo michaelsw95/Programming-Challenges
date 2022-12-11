c38bb34b20838c227dec5c9accf22d845b91d521
Michael Hancock <michael.hancock@protonmail.com>
2022-12-11 09:26:49 +0000

    Add solution for AdventOfCode2022 Day 10 Part 1

---
+++ AdventOfCode2022/Day-10-Part-01/Program.cs
@@ -0,0 +1,42 @@
+﻿var instructions = File.ReadAllLines("input.txt");
+
+var executionPosition = 0;
+var signalStrength = new List<int>();
+var registerValue = 1;
+var cycleCount = 1;
+
+var executingInstruction = (Value: 0, Wait: 0); 
+while (executionPosition < instructions.Length || executingInstruction.Wait > 0)
+{
+    if (executingInstruction.Wait > 0)
+    {
+        executingInstruction.Wait--;
+
+        signalStrength.Add(cycleCount * registerValue);
+
+        if (executingInstruction.Wait == 0)
+        {
+            registerValue += executingInstruction.Value;
+        }
+
+        cycleCount++;
+        continue;
+    }
+
+    var instruction = instructions[executionPosition];
+    var instructionParts = instruction.Split(' ');
+    
+    if (instruction.StartsWith("addx"))
+    {
+        executingInstruction = (Value: int.Parse(instructionParts[1]), Wait: 1);
+    }
+
+    signalStrength.Add(cycleCount * registerValue);
+
+    executionPosition++;
+    cycleCount++;
+}
+
+var significantSignals = signalStrength[19] + signalStrength[59] + signalStrength[99] + signalStrength[139] + signalStrength[179] + signalStrength[219];
+
+Console.WriteLine($"Day 10 - Part 1: {significantSignals}");
