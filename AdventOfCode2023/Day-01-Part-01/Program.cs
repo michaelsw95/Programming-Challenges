19f0acf0a233bd9d98a6f9934ed44d73192dbb2f
Michael Hancock <michael.hancock@protonmail.com>
2023-12-01 12:22:15 +0000

    Add solution for AdventOfCode2023 Day 1 Part 1

---
+++ AdventOfCode2023/Day-01-Part-01/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+
+var sumOfCalibrationValues = File.ReadAllLines("./input.txt")
+    .Select(calibration => {
+        var digits = calibration.Where(value => char.IsDigit(value));
+
+        return $"{digits.First()}{digits.Last()}";
+    })
+    .Sum(calibartionValue => int.Parse(calibartionValue));
+
+Console.WriteLine($"Day 1 - Part 1: {sumOfCalibrationValues}");
