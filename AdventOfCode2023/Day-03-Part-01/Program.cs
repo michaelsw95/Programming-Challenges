32f86ce11ee84d0b6af464203c208bf7d3a8f11d
Michael Hancock <michael.hancock@protonmail.com>
2023-12-03 10:12:02 +0000

    Add solution for AdventOfCode2023 Day 3 Part 1

---
+++ AdventOfCode2023/Day-03-Part-01/Program.cs
@@ -0,0 +1,66 @@
+﻿var engineSchematic = File.ReadAllLines("./input.txt").Select(line => line.ToCharArray()).ToArray();
+
+var numbersNextToSymbols = new List<int>();
+
+for (var i = 0; i < engineSchematic.Length; i++)
+{
+    var readIndex = 0;
+
+    while (readIndex < engineSchematic[i].Length)
+    {
+        var candidateNumberCharacters = new List<char>();
+        var hasNeighbouringSymbol = false;
+        if (char.IsDigit(engineSchematic[i][readIndex]))
+        {
+            do
+            {
+                candidateNumberCharacters.Add(engineSchematic[i][readIndex]);
+
+                if (!hasNeighbouringSymbol && GetHasNeighbouringSymbol(engineSchematic, i, readIndex))
+                {
+                    hasNeighbouringSymbol = true;
+                }
+                
+                readIndex++;
+            } while (readIndex < engineSchematic[i].Length && char.IsDigit(engineSchematic[i][readIndex]));
+
+            if (hasNeighbouringSymbol)
+            {
+                numbersNextToSymbols.Add(int.Parse(new string(candidateNumberCharacters.ToArray())));
+            }
+        }
+
+        readIndex++;
+    }
+}
+
+Console.WriteLine($"Day 3 - Part 1: {numbersNextToSymbols.Sum()}");
+
+bool GetHasNeighbouringSymbol(char[][] schematic, int xPosition, int yPosition)
+{
+    var positionsToCheck = new List<(int X, int Y)>
+    {
+        (xPosition + 1, yPosition),
+        (xPosition - 1, yPosition),
+        (xPosition, yPosition + 1),
+        (xPosition, yPosition - 1),
+        (xPosition + 1, yPosition + 1),
+        (xPosition + 1, yPosition - 1),
+        (xPosition - 1, yPosition + 1),
+        (xPosition - 1, yPosition - 1),
+    };
+
+    return positionsToCheck.Any(position => PositionContainsSymbol(schematic, position.X, position.Y));
+}
+
+bool PositionContainsSymbol(char[][] schematic, int x, int y)
+{
+    if (x < 0 || y < 0 || x >= schematic.Length || y >= schematic[x].Length)
+    {
+        return false;
+    }
+
+    var charAtPosition = schematic[x][y];
+
+    return charAtPosition != '.' && !char.IsDigit(charAtPosition);
+}
