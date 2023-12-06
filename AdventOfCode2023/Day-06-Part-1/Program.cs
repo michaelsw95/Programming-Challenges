0e799405b8c21c36ae749024a24212dc6bbbc2d3
Michael Hancock <michael.hancock@protonmail.com>
2023-12-06 20:38:58 +0000

    Add solution for AdventOfCode2023 Day 6 Part 1

---
+++ AdventOfCode2023/Day-06-Part-1/Program.cs
@@ -0,0 +1,40 @@
+﻿var raceInput = File.ReadAllLines("./input.txt")
+    .Select(line =>
+        line.Split(':')
+            .Last()
+            .Split(' ')
+            .Where(raceValue => !string.IsNullOrWhiteSpace(raceValue))
+            .Select(int.Parse)
+        );
+
+var marginOfError = raceInput
+    .First()
+    .Zip(raceInput.Last(), (time, distance) => new Race(time, distance))
+    .Select(GetNumberOfButtonLengthsToWin)
+    .Aggregate(1, (raceOutputOne, raceOutputTwo) => raceOutputOne * raceOutputTwo);
+
+Console.WriteLine($"Day 6 - Part 1: {marginOfError}");
+
+int GetNumberOfButtonLengthsToWin(Race race)
+{
+    var candidateHoldButtonTimes = Enumerable.Range(0, race.Time);
+    var numberOfButtonLengthsToWin = 0;
+
+    foreach (var buttonTime in candidateHoldButtonTimes)
+    {
+        var remainingTime = race.Time - buttonTime;
+
+        var newDistancedTravelled = buttonTime * remainingTime;
+
+        if (newDistancedTravelled > race.Distance)
+        {
+            numberOfButtonLengthsToWin++;
+        }
+    }
+
+    return numberOfButtonLengthsToWin;
+}
+
+Console.WriteLine("Hello, World!");
+
+record Race(int Time, int Distance);
