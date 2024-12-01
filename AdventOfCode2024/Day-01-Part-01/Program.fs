2a14b6403019a4799a5afd1698de6c149839f14d
Michael Hancock <michael.hancock@protonmail.com>
2024-12-01 19:35:36 +0000

    Add solution for AdventOfCode2024 Day 1 Part 1

---
+++ AdventOfCode2024/Day-01-Part-01/Program.fs
@@ -0,0 +1,19 @@
+﻿open System.IO;
+
+let input = File.ReadAllLines("./input.txt")
+
+let leftSide, rightSide =
+    input
+    |> Array.map (fun line ->
+        let parts = line.Split("  ")
+        int (parts[0]), int parts[1])
+    |> Array.unzip
+    
+let leftSideSorted = leftSide |> Array.sort
+let rightSideSorted = rightSide |> Array.sort
+
+let difference =
+    Array.zip leftSideSorted rightSideSorted
+    |> Array.sumBy (fun (left, right) -> abs (left - right))
+        
+printfn $"Day 1 - Part 1: {difference}"
